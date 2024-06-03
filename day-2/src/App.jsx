b5609e6c2c24dc0e26b9671b16298268ceda2643
Patadiyanevil <patadiyanevil77@gmail.com>
2024-06-03 16:22:54 +0530

    React Webpage

---
+++ day-2/src/App.jsx
@@ -0,0 +1,161 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <div class="logo"><a href="">T SHOP</a></div>
+        <nav>
+            <ul>
+                <li>New</li>
+                <li>Men</li>
+                <li>Women</li>
+                <li class="login">Log in</li>
+            </ul>
+        </nav>
+    </header>
+
+
+    <div class="section2">
+        <div class="heading">
+            <h1>T SHOP</h1>
+        </div>
+        <div class="slogan">
+            <div class="subslogan">
+            <h1>there is one for everyone</h1>
+             </div>
+        </div>
+        <div class="img">
+            <img src="image/img1.jpg" alt=""/>
+        </div>
+        <div class="specialimg">
+            <img src="image/img2.jpg" alt=""/>
+        </div>
+        <div class="img_3">
+            <img src="image/img3.jpg" alt=""/>
+        </div>
+    </div>
+
+
+
+    <div class="section3">
+        <h1>new drops</h1>
+        <div class="img">
+            <div class="image">
+                <img src="image/3-1.jpg" alt=""/>
+            </div>
+            <h1>I'm a Product</h1>
+                <h1>$15.00</h1>
+        </div>
+        <div class="img">
+            <div class="image">
+                <img src="image/3-2.jpg" alt=""/>
+                </div>
+                <h1>I'm a Product</h1>
+                <h1>$15.00</h1>
+        </div>
+        <div class="img">
+            <div class="image">
+                <img src="image/3-3.jpg" alt=""/>
+                </div>
+                <h1>I'm a Product</h1>
+                <h1>$15.00</h1>
+        </div>
+        <div class="img">
+            <div class="image">
+                <img src="image/3-4.jpg" alt=""/>
+               </div>
+               <h1>I'm a Product</h1>
+                <h1>$15.00</h1>
+        </div>
+        <div class="img">
+            <div class="image">
+                <img src="image/3-5.jpg" alt=""/>
+            </div>
+            <h1>I'm a Product</h1>
+                <h1>$15.00</h1>
+        </div>
+        <div class="img">
+            <div class="image">
+                <img src="image/3-6.jpg" alt=""/>
+              </div>
+              <h1>I'm a Product</h1>
+                <h1>$15.00</h1>
+        </div>
+        <div class="img">
+            <div class="image">
+                <img src="image/3-7.jpg" alt=""/>
+              </div>
+              <h1>I'm a Product</h1>
+                <h1>$15.00</h1>
+        </div>
+        <div class="img">
+            <div class="image">
+                <img src="image/3-8.jpg" alt=""/>
+                </div>
+                <h1>I'm a Product</h1>
+                <h1>$15.00</h1>
+        </div>
+
+        <button>Shop New</button>
+    </div>
+
+
+    <div class="section4">
+        <div class="text">
+            <i class="ri-guide-fill"></i>
+            <h1>sale is on!</h1>
+            <h3>25% off</h3>
+            <h5>25% off sitewide using TEES25 at checkout code</h5>
+        </div>
+        <div class="img">
+            <img src="image/sec4_1.jpg" alt=""/>
+        </div>
+    </div>
+
+
+    <div class="section5">
+        <i class="ri-arrow-right-fill"></i>
+        <h1>get 10% off on your order</h1>
+        <i class="ri-arrow-left-fill"></i>
+    </div>
+
+
+
+
+    <footer>
+        <div class="heading">
+            <h1>T SHOP</h1>
+            <h1>info@mysite.com</h1>
+            <h1>tel:123-456-7890</h1>
+        </div>
+
+        <div class="shop">
+            <h1>SHOP</h1>
+            <h2>_MEN</h2>
+            <h2>_WOMEN</h2>
+            <h2>_NEW</h2>
+        </div>
+
+
+
+        <div class="about">
+            <h1>our store</h1>
+            <h2>_About us</h2>
+            <h2>_Subscribe</h2>
+            <h2>_FAQ</h2>
+        </div>
+
+<div class="tandc">
+    <h1>terms and conditions</h1>
+    <h2>_Store policy</h2>
+    <h2>_Shipping & returns</h2>
+    <h2>_Payment methods</h2>
+</div>
+    </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ day-2/src/header.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function Header()
+{
+    <header className='header'>
+
+    </header>
+}
+
+export default Header;
